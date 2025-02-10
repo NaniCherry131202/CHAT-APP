c93b8fcd0650ab86250fd9b0ae2f8675eab6b2eb
CHARAN PEDDI <charanpeddi37@gmail.com>
2025-02-10 17:27:20 +0530

    Error set

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,12 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-const daisyui = require("daisyui");
+import daisyui from "daisyui";
 
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./frontend/index.html",
-    "./frontend/src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
